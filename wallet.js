83de670fe03c6104492b8a230c13055a34a167c3
Tim Lee <tim@blockchain.info>
2014-03-26 21:48:18 -0700

    remove unuse MyWallet.changeView

--- wallet.js
+++ wallet.js
@@ -3564,10 +3564,6 @@ var MyWallet = new function() {
         });
     }
 
-    this.changeView = function(id) {
-        changeView(id);
-    }
-
     function changeView(id) {
         if (isMobile && id.attr('id') == "home-intro")
             id = $("#my-transactions");
